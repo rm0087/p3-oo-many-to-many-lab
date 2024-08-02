ec0a02911aeac0566c5a59d90274d2a2147aa220
rm0087 <michetti.ray@gmail.com>
2024-08-01 23:54:53 -0400

    completed

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,104 @@
 class Author:
-    pass
+    all = []
+    
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+    
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+    
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+
+    def total_royalties(self):
+        total = 0
+        for contract in self.contracts():
+            total += contract.royalties
+        return total
+
+            
 
 
 class Book:
-    pass
+    all = []
+    
+    def __init__(self, title):
+        self.title = title
+        Book.all.append(self)
 
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+    
+    def authors(self):
+        return [contract.author for contract in self.contracts() if contract.book == self ]
 
 class Contract:
-    pass
+    all = []
+    
+    def __init__(self, author, book, date:str, royalties:int):
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        Contract.all.append(self)
+
+    @property ## Contract.author ###############################################################################
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, author):
+        if isinstance(author, Author):
+            self._author = author
+        else:
+            raise Exception
+
+    @property ## Contract.book ###############################################################################
+    def book(self):
+        return self._book
+
+    @book.setter
+    def book(self, book):
+        if isinstance(book, Book):
+            self._book = book
+        else:
+            raise Exception
+    
+
+    @property ## Contract.date ###############################################################################
+    def date(self):
+        return self._date
+
+    @date.setter
+    def date(self, date):
+        if isinstance(date, str):
+            self._date = date
+        else:
+            raise Exception
+
+    @property ## Contract.royalties
+    def royalties(self):
+        return self._royalties
+    
+    @royalties.setter
+    def royalties(self, royalties):
+        if isinstance(royalties, int):
+            self._royalties = royalties
+        else:
+            raise Exception
+        
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in cls.all if contract.date == date]
+    
+
+
+## TEST DATA ###############################################################################
+author1 = Author("Joe")
+book1 = Book("The Life of a Schmuck")
+contract1 = Contract(author1, book1, "01/01/2024", 10)
+contract2 = Contract(author1, book1, "01/01/2024", 20)
